662c2d1e9047be46bae6710c8c32774e6f36f0f2
Uzair Tariq <uzairtariq6949@gmail.com>
2025-03-17 02:27:00 +0500

    DOM

--- javascript/index.js
+++ javascript/index.js
@@ -28,3 +28,54 @@ document.getElementById("dynamic-text").classList.add("cursor");
 
 // Start typing effect
 typeEffect();
+
+// services Cards
+let services = [
+  { id:1,
+    img:"Images/Architecture.svg",
+    card_title:"Architecture",
+    card_text:"At The Capture Construction, our architectural services focus on innovative designs that balance functionality and aesthetics. We work closely with clients to develop creative solutions tailored to their vision and project requirements."},
+  { id:2,
+    img:"Images/Construction.svg",
+    card_title:"Construction",
+    card_text:"We provide high-quality construction services for residential, commercial, and industrial projects, ensuring timely delivery and adherence to safety standards. Our experienced team handles every detail, from planning to execution, guaranteeing durability and cost-efficiency."},
+  { id:3,
+    img:"Images/Turnkeyprojects.svg",
+    card_title:"Turnkey Projects",
+    card_text:"Our Turnkey Project service offers end-to-end solutions, managing everything from design to completion for a hassle-free experience. We take full responsibility for delivering projects on time, within budget, and ready for immediate use."},    
+  { id:4,
+    img:"Images/Interior Designing.svg",
+    card_title:"Interior Designing",
+    card_text:"We transform spaces with our creative Interior Designing services, combining functionality with style to create visually appealing environments. Our team carefully selects materials and finishes that align with the client’s vision and the overall design concept."},
+  { id:5,
+    img:"Images/Renovation.svg",
+    card_title:"Renovation",
+    card_text:"Our Renovation services breathe new life into existing spaces, modernizing and upgrading them for improved functionality and aesthetics. Whether it's a minor update or a major overhaul, we ensure minimal disruption and high-quality results."},
+  { id:6,
+    img:"Images/B2B.svg",
+    card_title:"Business to Business",
+    card_text:"At The Capture Construction, our architectural services focus on innovative designs that balance functionality and aesthetics. We work closely with clients to develop creative solutions tailored to their vision and project requirements."},
+  ]
+
+for (let service = 0; service <services.length; service++ ){
+  document.getElementById("services-row").innerHTML+=`
+
+            <div class="col-12 col-md-4 col-sm-12">
+            <div class="card h-100 services_card">
+              <img
+                src="${services[service].img}"
+                class="card-img-top"
+                alt="Architecture"
+              />
+              <div class="card-body">
+                <h5 class="card-title d-flex justify-content-center">
+                ${services[service].card_title}
+                </h5>
+                <p class="card-text">
+                ${services[service].card_text}
+                </p>
+              </div>
+            </div>
+          </div>
+  `
+}
