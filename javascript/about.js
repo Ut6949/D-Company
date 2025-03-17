036da977f263660677c0ab35f7fe657338189a65
Uzair Tariq <uzairtariq6949@gmail.com>
2025-03-18 03:48:41 +0500

    c

---
+++ javascript/about.js
@@ -0,0 +1,46 @@
+let card = [
+    {
+    id:1,
+    img: 'Images/Farhan Profile.svg',
+    name:'Mr. Farhan-Ul-Haq',
+    des:'Managing Director / 3D Visualizer',
+    intro:'Is a seasoned <strong>3D Visualizer</strong> with vast experience in architectural visualization and construction design.'},
+    {
+    id:2,
+    img: 'Images/Farhan Profile.svg',
+    name:'Mr. Muhammad Haris Mughal',
+    des:'Architect /Interior Designer',
+    intro:'An accomplished <strong>Architect and Interior Designer</strong>, brings creativity and expertise to innovative architectural and interior design solutions.'},
+    {
+    id:3,
+    img: 'Images/Farhan Profile.svg',
+    name:'Mr. Naveed Saqib',
+    des:'Civil Engineer',
+    intro:'A skilled <strong>Civil Engineer</strong>, brings expertise in structural design and construction, ensuring durable and efficient engineering solutions.'
+    },
+    {
+    id:4,
+    img: 'Images/Farhan Profile.svg',
+    name:'Mr. Hamza Jameel',
+    des:'Site Surveyor/Draftsman',
+    intro:'A proficient <strong>Site Surveyor and Draftsman</strong>, specializes in precise site assessments and detailed technical drawings for accurate project execution.'
+    }]
+
+    for(let cards=0; cards<card.length; cards++){
+        document.getElementById("profile_card").innerHTML+=`
+        <div class="col-md-6 col-sm-12 d-flex g-4" id="profile_card">
+            <img
+            src="${card[cards].img}"
+            alt=""
+            class="img-fluid rounded-start-3"
+            height="200px"  
+            width="200px"
+          />
+          <div class="card-body bg-light rounded-end-3 p-3">
+            <h5 class="card-title ">${card[cards].name}</h5>
+            <hr />
+            <h6 class="card-subtitle mb-2 text-muted">${card[cards].des}</h6>
+            <p class="card-text">${card[cards].intro}</p>
+          </div>
+          </div>
+        ` }
