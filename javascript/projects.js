4f55564960b394f7d462ae866fc9c7e5b9afb22e
Uzair Tariq <uzairtariq6949@gmail.com>
2025-03-29 03:37:30 +0500

    s

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,12 +1,28 @@
-$(document).ready(function() {
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-    $(`[unique-script-id="w-w-dm-id"] .btn-box`).click(function() {
-      $(this).parent().children(".overlay").show();
-  
-    });
-  
-  
-    $(`[unique-script-id="w-w-dm-id"] .close`).click(function() {
-      $(".overlay").hide();
-    });
+// Pause autoplay on hover
+document
+  .querySelector(".swiper")
+  .addEventListener("mouseenter", function () {
+    swiper.autoplay.stop();
+  });
+
+document
+  .querySelector(".swiper")
+  .addEventListener("mouseleave", function () {
+    swiper.autoplay.start();
   });
